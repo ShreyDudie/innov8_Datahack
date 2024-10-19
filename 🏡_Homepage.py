a6d27947131d061d6f895d6f36c7650540a00c19
AnonymousPotato <118345746+VrajVed@users.noreply.github.com>
2024-10-19 21:18:55 +0530

    Update 🏡_Homepage.py

--- 🏡_Homepage.py
+++ 🏡_Homepage.py
@@ -3,7 +3,20 @@ import streamlit as st
 st.set_page_config(page_title='Flight Quest', page_icon='🏡')
 st.title('Flight Dashboard')
 st.write('Comprehensive solution to flights')
-container1 = st.container(height=100)
-container1.write("This is inside the container")
+
+
+container1 = st.container(height=125)
+container1.write("Explore real-time flight information, analyze delays, and gain insights into weather conditions that may affect your journey.\n\n" "Our services are listed below:")
+
+
 st.sidebar.success("Choose your page")
+if st.button('ETA Analysis'):
+    st.switch_page('pages/3_⌚_ETA.py')
+
+if st.button('Wind Conditions'):
+    st.switch_page('pages/4_🍃_Wind.py')
+
+if st.button('See Past Flights'):
+    st.switch_page('pages/1_✈️_Flights.py')
+
 
